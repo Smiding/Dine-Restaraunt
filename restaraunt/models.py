02f635c1972a302189abe16de0cb7a5e3533a610
Smiding <Eriksmiding@gmail.com>
2023-06-17 01:04:44 +0500

    bug fix

--- restaraunt/models.py
+++ restaraunt/models.py
@@ -28,32 +28,6 @@ class Booking(models.Model):
     person = models.IntegerField(null=False, blank=False)
     table = models.ForeignKey(Table, null=False, blank=False, on_delete=models.CASCADE)
 
-    # def save(self, *args, **kwargs):
-    #     self.validate_date_time( *args, **kwargs)
-    #     available_tables = Table.objects.filter(status='Available').order_by('Capacity')
-    #     for table in available_tables:
-    #         if table.Capacity >= self.person:
-    #             # Check if the table is already booked within one hour of the same day
-    #             existing_bookings = Booking.objects.filter(
-    #                 table=table,
-    #                 date=self.date,
-    #                 time__gte=datetime.datetime.combine(self.date, self.time) - datetime.timedelta(hours=1),
-    #                 time__lt=datetime.datetime.combine(self.date, self.time) + datetime.timedelta(hours=1)
-    #             ).exclude(pk=self.pk)  # Exclude the current booking if it's being updated
-    #
-    #             if not existing_bookings:
-    #                 self.table = table
-    #                 break
-    #     else:
-    #         raise ValidationError("No available table found at the specified time")
-    #     super(Booking, self).save(*args, **kwargs)
-    #
-    # def validate_date_time(self, *args, **kwargs):
-    #     curr_datetime = datetime.datetime.combine(self.date, self.time)
-    #     if curr_datetime > datetime.datetime.now():
-    #         return
-    #     raise ValidationError("Invalid Date & Time.")
-
     def save(self, *args, **kwargs):
         self.validate_date_time()
         self.assign_table()
@@ -69,9 +43,9 @@ class Booking(models.Model):
             raise ValidationError("Invalid Date & Time.")
 
     def assign_table(self):
-        available_tables = Table.objects.filter(status='Available').order_by('capacity')
+        available_tables = Table.objects.filter(status='Available').order_by('Capacity')
         for table in available_tables:
-            if table.capacity >= self.person:
+            if table.Capacity >= self.person:
                 booking_start_time = self.datetime - timezone.timedelta(hours=1)
                 booking_end_time = self.datetime + timezone.timedelta(hours=1)
                 existing_bookings = Booking.objects.filter(
